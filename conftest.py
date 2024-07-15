63f020f46c4874101b134f61991c38db78997c40
Pavel <pashabugakov@yandex.ru>
2024-07-15 17:36:03 +0300

    Edit docker-compose, docker and config

--- conftest.py
+++ conftest.py
@@ -1,8 +1,6 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope="function", autouse=True)
@@ -14,7 +12,8 @@ def driver(request):
     # options.add_argument('--start-maximized')  # fullscreen
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument("--window-size=1920x1080")  # size of the browser
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver = webdriver.Chrome(options=options)
+    # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)  # driver Chrome
     request.cls.driver = driver
     yield driver
-    driver.close()
+    driver.quit()
